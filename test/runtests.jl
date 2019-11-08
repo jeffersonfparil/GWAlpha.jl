f913c67c6993e4566240784882b86242f4690254
jeffersonfparil <jeffersonparil@gmail.com>
2019-11-08 22:31:07 +1100

    testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,12 @@
 using Test
+using DelimitedFiles
 using GWAlpha
 
-DIR = replace(dirname(pathof(GWAlpha)), "/src" => "/test")
-geno_sync_fname = string(DIR, "/LOLIUM_1rep_10QTL_0.001mr_0.25fgs_0.00bgs_1grad_g500_p16_POOLS_GENO.sync")
-pheno_py_fname = string(DIR, "/LOLIUM_1rep_10QTL_0.001mr_0.25fgs_0.00bgs_1grad_g500_p16_POOLS_PHENO.py")
-pheno_csv_fname = string(DIR, "/LOLIUM_1rep_10QTL_0.001mr_0.25fgs_0.00bgs_1grad_g500_p16_POOLS_PHENO.csv")
-covariate_fname = string(DIR, "/LOLIUM_1rep_10QTL_0.001mr_0.25fgs_0.00bgs_1grad_g500_p16_POOLS_GENO_COVARIATE_FST.csv")
+# DIR = replace(dirname(pathof(GWAlpha)), "/src" => "/test")
+# geno_sync_fname = string(DIR, "/LOLIUM_1rep_10QTL_0.001mr_0.25fgs_0.00bgs_1grad_g500_p16_POOLS_GENO.sync")
+# pheno_py_fname = string(DIR, "/LOLIUM_1rep_10QTL_0.001mr_0.25fgs_0.00bgs_1grad_g500_p16_POOLS_PHENO.py")
+# pheno_csv_fname = string(DIR, "/LOLIUM_1rep_10QTL_0.001mr_0.25fgs_0.00bgs_1grad_g500_p16_POOLS_PHENO.csv")
+# covariate_fname = string(DIR, "/LOLIUM_1rep_10QTL_0.001mr_0.25fgs_0.00bgs_1grad_g500_p16_POOLS_GENO_COVARIATE_FST.csv")
 
 function runGWAlpha(filename_sync::String, filename_phen::String, MAF::Float64, DEPTH::Int64; MODEL="FIXED_GWAlpha"::String, COVARIATE=nothing)
     GWAlpha.PoolGPAS(filename_sync, filename_phen, MAF, DEPTH, MODEL=MODEL, COVARIATE=COVARIATE)
@@ -25,4 +26,4 @@ Test.@test runGWAlpha(geno_sync_fname,
                       0.01,
                       10,
                       MODEL="FIXED_RR",
-                      COVARIATE=covariate_fname) == 0
+                      COVARIATE=DelimitedFiles.readdlm(covariate_fname, ',')) == 0
