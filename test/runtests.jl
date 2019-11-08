7b4d0d3c0bc22cfc9bb911cf3f78d886d0f3918b
jeffersonfparil <jeffersonparil@gmail.com>
2019-11-08 16:59:07 +1100

    testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,22 @@
 using Test
 using GWAlpha
 
-@test println("Testing!")
+function runGWAlpha(filename_sync::String, filename_phen::String, MAF::Float64, DEPTH::Int64; MODEL="FIXED_GWAlpha", COVARIATE=nothing)
+    PoolGPAS(filename_sync::String, filename_phen::String, MAF::Float64, DEPTH::Int64; MODEL="FIXED_GWAlpha", COVARIATE=nothing)
+    return(0)
+end
+
+### GWAlpha for GWAS
+Test.@test runGWAlpha("UG_MAPQ20_BASQ20_MAF0.001_DEPTH10.sync",
+                      "UG_pheno.py",
+                      0.01,
+                      10,
+                      MODEL="FIXED_GWAlpha",
+                      COVARIATE=nothing) == 0
+### GWAlpha for GP
+Test.@test runGWAlpha("UG_MAPQ20_BASQ20_MAF0.001_DEPTH10.sync",
+                      "UG_pheno.csv",
+                      0.01,
+                      10,
+                      MODEL="FIXED_RR",
+                      COVARIATE="UG_MAPQ20_BASQ20_MAF0.001_DEPTH10_COVARIATE_FST.csv") == 0
