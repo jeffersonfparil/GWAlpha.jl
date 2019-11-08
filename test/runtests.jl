416444f02009c32105ea4bbf23dcf70e1f83dae9
jeffersonfparil <jeffersonparil@gmail.com>
2019-11-08 22:36:53 +1100

    testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,6 +8,11 @@ using GWAlpha
 # pheno_csv_fname = string(DIR, "/LOLIUM_1rep_10QTL_0.001mr_0.25fgs_0.00bgs_1grad_g500_p16_POOLS_PHENO.csv")
 # covariate_fname = string(DIR, "/LOLIUM_1rep_10QTL_0.001mr_0.25fgs_0.00bgs_1grad_g500_p16_POOLS_GENO_COVARIATE_FST.csv")
 
+geno_sync_fname = "LOLIUM_1rep_10QTL_0.001mr_0.25fgs_0.00bgs_1grad_g500_p16_POOLS_GENO.sync"
+pheno_py_fname = "LOLIUM_1rep_10QTL_0.001mr_0.25fgs_0.00bgs_1grad_g500_p16_POOLS_PHENO.py"
+pheno_csv_fname = "LOLIUM_1rep_10QTL_0.001mr_0.25fgs_0.00bgs_1grad_g500_p16_POOLS_PHENO.csv"
+covariate_fname = "LOLIUM_1rep_10QTL_0.001mr_0.25fgs_0.00bgs_1grad_g500_p16_POOLS_GENO_COVARIATE_FST.csv"
+
 function runGWAlpha(filename_sync::String, filename_phen::String, MAF::Float64, DEPTH::Int64; MODEL="FIXED_GWAlpha"::String, COVARIATE=nothing)
     GWAlpha.PoolGPAS(filename_sync, filename_phen, MAF, DEPTH, MODEL=MODEL, COVARIATE=COVARIATE)
     return(0)
