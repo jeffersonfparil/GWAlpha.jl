d57c3ac6cc73e85c1cb740be490688952bcc62cd
jeffersonfparil <jeffersonparil@gmail.com>
2020-04-15 01:04:54 +1000

    restructuring and streamlining and testing and more tests tomorrow

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,12 @@
 using Test
-using DelimitedFiles
 using GWAlpha
 
 geno_sync_fname = "test.sync"
 pheno_py_fname = "test.py"
 pheno_csv_fname = "test.csv"
 
-function runGWAlpha(filename_sync::String, filename_phen::String, MAF::Float64, DEPTH::Int64, MODEL::String, COVARIATE::Any, FPR::Float64, PARALLEL::Bool)
-    GWAlpha.PoolGPAS(filename_sync, filename_phen, MAF=MAF, DEPTH=DEPTH, MODEL=MODEL, COVARIATE=COVARIATE, FPR=FPR, PARALLEL=PARALLEL)
+function runGWAlpha(filename_sync::String, filename_phen::String, MAF::Float64, DEPTH::Int64, MODEL::String)
+    GWAlpha.PoolGPAS(filename_sync=filename_sync, filename_phen=filename_phen, maf=MAF, depth=DEPTH, model=MODEL, plot=true)
     return(0)
 end
 
@@ -16,37 +15,25 @@ Test.@test runGWAlpha(geno_sync_fname,
                       pheno_py_fname,
                       0.01,
                       10,
-                      "FIXED_GWAlpha",
-                      nothing,
-                      0.01,
-                      false) == 0
+                      "GWAlpha") == 0
 
 ### GWAlpha for GP
-pool_sizes = convert(Array{Int}, DelimitedFiles.readdlm(pheno_csv_fname, ',')[:,1])
-Test.@test GWAlpha.poolFST_module.Fst_pairwise(sync_fname=geno_sync_fname, window_size=100000, pool_sizes=pool_sizes, METHOD="Hivert") == 0
-covariate_fname = "test_COVARIATE_FST.csv"
 Test.@test runGWAlpha(geno_sync_fname,
                       pheno_csv_fname,
                       0.01,
                       10,
-                      "FIXED_RR",
-                      DelimitedFiles.readdlm(covariate_fname, ','),
-                      0.01,
-                      false) == 0
+                      "REML_GLMNET") == 0
 
 ### GWAlpha for GWAS (parallel)
 using Distributed
 Distributed.addprocs(length(Sys.cpu_info()))
 @everywhere using GWAlpha
-function runGWAlpha(filename_sync::String, filename_phen::String, MAF::Float64, DEPTH::Int64, MODEL::String, COVARIATE::Any, FPR::Float64, PARALLEL::Bool)
-    GWAlpha.PoolGPAS(filename_sync, filename_phen, MAF=MAF, DEPTH=DEPTH, MODEL=MODEL, COVARIATE=COVARIATE, FPR=FPR, PARALLEL=PARALLEL)
+function runGWAlpha(filename_sync::String, filename_phen::String, MAF::Float64, DEPTH::Int64, MODEL::String)
+    GWAlpha.PoolGPAS(filename_sync=filename_sync, filename_phen=filename_phen, maf=MAF, depth=DEPTH, model=MODEL, plot=true)
     return(0)
 end
 Test.@test runGWAlpha(geno_sync_fname,
-                    pheno_py_fname,
-                    0.01,
-                    10,
-                    "FIXED_GWAlpha",
-                    nothing,
-                    0.01,
-                    true) == 0
+                      pheno_py_fname,
+                      0.01,
+                      10,
+                      "GWAlpha") == 0
