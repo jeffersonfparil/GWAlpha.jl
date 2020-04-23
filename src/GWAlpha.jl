2324b042d16e58b09479b853f48806bb36fe97d4
jeffersonfparil <jeffersonparil@gmail.com>
2020-04-23 17:16:31 +1000

    proper testing for filtering and parsing

--- src/GWAlpha.jl
+++ src/GWAlpha.jl
@@ -154,12 +154,13 @@ function PoolGPAS(;filename_sync::String, filename_phen::String, maf::Float64=0.
 			filename_sync_filtered = filename_sync
 		end
 		### parse the filtered sync file if the input file has not yet been parsed
+		filename_sync_parsed = string(join(split(filename_sync, ".")[1:(end-1)], '.'), "_ALLELEFREQ.csv")
 		if !isfile(filename_sync_parsed)
 			println(string("Parsing the filtered genotype data (sync format): \"", filename_sync, "\""))
 			sync_processing_module.sync_parse(filename_sync_filtered)
 			filename_sync_parsed = string(join(split(filename_sync_filtered, ".")[1:(end-1)], '.'), "_ALLELEFREQ.csv")
 		else
-			filename_sync_parsed = string(join(split(filename_sync, ".")[1:(end-1)], '.'), "_ALLELEFREQ.csv")
+			filename_sync_parsed = filename_sync_parsed
 		end
 		println(string("Load the filtered and parsed genotype data (csv format): \"", filename_sync_parsed, "\"."))
 		GENO = DelimitedFiles.readdlm(filename_sync_parsed, ',')
